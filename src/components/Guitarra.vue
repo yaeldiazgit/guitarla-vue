a0e61f8c25db6cf41359d6e0e14e49a3e5d94bc8
Yael Díaz <yaeldiaz.ygds@gmail.com>
2024-03-14 01:26:39 -0600

    Primer commit

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+const numero = ref(0);
+    const props = defineProps({
+        guitarra: {
+            type: Object,
+            required: true
+        }
+    });
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" :src="`/img/${guitarra.imagen}.jpg`" :alt="guitarra.nombre">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p> {{ guitarra.descripcion }}</p>
+            <p>{{ numero }}</p>
+            <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+            <button type="button" class="btn btn-dark w-100" @click="numero++" >Agregar al Carrito</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
