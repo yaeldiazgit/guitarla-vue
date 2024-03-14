9103a4f7059af2ddfd0ad1c28adf2ea79f11626e
Yael Díaz <yaeldiaz.ygds@gmail.com>
2024-03-14 01:31:24 -0600

    Se agrega msg

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -17,7 +17,7 @@ const numero = ref(0);
         <div class="col-8">
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
             <p> {{ guitarra.descripcion }}</p>
-            <p>{{ numero }}</p>
+            <p>{{ numero }} disp </p>
             <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
             <button type="button" class="btn btn-dark w-100" @click="numero++" >Agregar al Carrito</button>
         </div>
